a77b294699cfde90582d97f3521c18533deff944
Samuel Person <samuel.person@associatedenvironmentalsystems.com>
2021-11-10 09:20:14 -0500

    Updated canvasHandler

--- server/handlers/canvasHandler.js
+++ server/handlers/canvasHandler.js
@@ -1,13 +1,14 @@
 const db = require('../models');
 
-exports.createCanvas = async (req, res, next) => {
+const createCanvas = async (req, res, next) => {
+    const { body } = req;
+    const copy = { ...body.canvas };
+    delete copy._id;
+    copy.user = body.user
     try {
-        let canvas = await db.Canvas.create({
-            canvasData: req.body.canvasData,
-            user: req.params.id
-        });
-        let foundUser = await db.User.findById(req.params.id);
-        foundUser.canvas.push(canvas.id);
+        let canvas = await db.Canvas.create(copy);
+        let foundUser = await db.User.findById(body.user);
+        foundUser.canvas.push(canvas._id);
         await foundUser.save();
         let foundCanvas = await db.Canvas.findById(canvas._id).populate('user', {
             username: true
@@ -18,21 +19,80 @@ exports.createCanvas = async (req, res, next) => {
     }
 }
 
-exports.getCanvas = async (req, res, next) => {
+const getCanvasList = async (req, res, next) => {
+    const { query } = req;
     try {
-        let canvas = await db.Canvas.find(req.params.canvas_id);
-        return res.status(200).json(canvas);
+        let response = await db.Canvas.find(
+            {
+                user: query.user_id,
+                deleted: false
+            }
+        )
+        return res.status(200).json(response)
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const getCanvas = async (req, res, next) => {
+    const { params } = req;
+    try {
+        let canvas = await db.Canvas.find({_id: params.canvas_id});
+        return res.status(200).json(canvas[0]);
     } catch (err) {
+        return next({
+            status: 400, 
+            message: err.message
+        })
+    }
+}
+
+const updateCanvas = async (req, res, next) => {
+    const { body, params } = req;
+    const {
+        shapeList,
+        canvasData,
+        editor,
+        info
+    } = body.canvas;
+    try {
+        let canvas = await db.Canvas.findOneAndUpdate(
+            { _id: params.canvas_id },
+            {
+                $set: {
+                    shapeList,
+                    canvasData,
+                    editor,
+                    info
+                }
+            },
+            {new: true}
+        )
+        res.status(200).json(canvas[0])
+    } catch (error) {
+        return next({
+            status: 400, 
+            message: err.message
+        })
+    }
+}
+
+const deleteCanvas = async (req, res, next) => {
+    const { params } = req;
+    try {
+        let canvas = await db.Canvas.findById(params.canvas_id);
+        canvas.deleted = true;
+        canvas.save();
+        return res.status(200).json(canvas);
+    } catch (error) {
         return next(err);
     }
 }
 
-exports.deleteCanvas = async (req, res, next) => {
-    try {
-        let foundCanvas = await db.Canvas.findById(req.params.canvas_id);
-        await foundCanvas.remove();
-        return res.status(200).json(foundMessage);
-    } catch (error) {
-        return next(err);
-    }
+module.exports = {
+    createCanvas,
+    getCanvas,
+    updateCanvas,
+    getCanvasList,
+    deleteCanvas
 }
